c4cc70d27755a6f54ac76da1c1ea7083eb3fbed7
melaku <genuieman@gmail.com>
2023-06-15 15:52:14 +0300

    Adding questions

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,12 +1,51 @@
 import React, { Fragment } from "react";
 import { Helmet } from "react-helmet";
 
+import questions from '../../assets/questions.json'
+import isEmpty from "../../utils/is-empty";
+
 class Play extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion: {},
+            answer: '',
+            numberofQuestions: 0,
+            numberofAnsweredQuestion: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            hints: 5,
+            fiftyFifty: 2,
+            usedFiftyFifty: false,
+            time: {}
+        }
+    }
+
+    componentDidMount() {
+        const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state
+        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion)
+    }
+
+    displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
+        let { currentQuestionIndex } = this.state;
+        if (!isEmpty(this.state.questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestion + 1];
+            previousQuestion = questions[currentQuestion - 1];
+            const answer = currentQuestion.answer
+            this.setState({
+                currentQuestion, nextQuestion, previousQuestion, answer
+            })
+        }
     }
     render() {
+        const { currentQuestion } = this.state
         return (
             <Fragment>
                 <Helmet><title>Quiz page</title></Helmet>
@@ -26,14 +65,14 @@ class Play extends React.Component {
                             <span className="right">2:15 <span className="mdi mdi-clock-outline mdi-24px"></span></span>
                         </p>
                     </div>
-                    <h5>Google was founded in what year?</h5>
+                    <h5>{currentQuestion.question}</h5>
                     <div className="options-container">
-                        <p className="option">1997</p>
-                        <p className="option">1998</p>
+                        <p className="option">{currentQuestion.optionA}</p>
+                        <p className="option">{currentQuestion.optionB}</p>
                     </div>
                     <div className="options-container">
-                        <p className="option">1999</p>
-                        <p className="option">2000</p>
+                        <p className="option">{currentQuestion.optionC}</p>
+                        <p className="option">{currentQuestion.optionD}</p>
                     </div>
                     <div className="button-container">
                         <button>Previous</button>
